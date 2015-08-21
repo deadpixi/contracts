7252c9d018e4a161c743702a81d6c44f3d60d907
Rob King <jking@deadpixi.com>
2015-08-21 16:15:02 -0500

    Renamed contracts to dpcontracts.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import contracts
+import dpcontracts
 
 setup(name="contracts",
       version="0.1.0",
@@ -9,14 +9,12 @@ setup(name="contracts",
       author_email="jking@deadpixi.com",
       url="https://github.com/deadpixi/contracts",
       description="A simple implementation of contracts for Python.",
-      py_modules=['contracts'],
+      py_modules=['dpcontracts'],
       long_description=contracts.__doc__,
       license="https://www.gnu.org/licenses/lgpl.txt",
-      classifiers="""
-Development Status :: 3 - Alpha
-Intended Audience :: Developers
-License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)
-Operating System :: OS Independent
-Programming Language :: Python
-Topic :: Software Development :: Libraries
-""")
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Libraries"])
