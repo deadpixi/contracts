5f3abf2d9abf8c0d468d044ad15e72a400080dfc
Rob King <jking@deadpixi.com>
2015-08-21 15:57:01 -0500

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="contracts",
+      version="0.1.0",
+      author="Rob King",
+      author_email="jking@deadpixi.com",
+      url="https://github.com/deadpixi/contracts",
+      description="A simple implementation of contracts for Python.",
+      py_modules=['contracts'])
