73fe48c9f806b2d1308168793bbc30c5b341f24c
Rob King <jking@deadpixi.com>
2015-08-21 16:08:27 -0500

    Added metadata.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import contracts
 
 setup(name="contracts",
       version="0.1.0",
@@ -8,4 +9,15 @@ setup(name="contracts",
       author_email="jking@deadpixi.com",
       url="https://github.com/deadpixi/contracts",
       description="A simple implementation of contracts for Python.",
-      py_modules=['contracts'])
+      py_modules=['contracts'],
+      long_description=contracts.__doc__,
+      license="https://www.gnu.org/licenses/lgpl.txt",
+      classifiers="""
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Libraries
+""")
+
