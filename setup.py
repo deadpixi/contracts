00e6372bca9d24e312f39ff7cfff49d37f01344a
Rob King <jking@deadpixi.com>
2015-08-24 17:14:46 -0500

    Added explicit compatibility classifiers for Python 2 and 3.

--- setup.py
+++ setup.py
@@ -17,4 +17,6 @@ setup(name="dpcontracts",
                    "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
                    "Operating System :: OS Independent",
                    "Programming Language :: Python",
+                   "Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 3",
                    "Topic :: Software Development :: Libraries"])
