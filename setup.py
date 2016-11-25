b2837fb7f83ea58a8b157fda5f47559b41ebf643
Rob King <jking@deadpixi.com>
2016-11-24 23:36:58 -0600

    Updated version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 import dpcontracts
 
 setup(name="dpcontracts",
-      version="0.1.0",
+      version="0.2.0",
       author="Rob King",
       author_email="jking@deadpixi.com",
       url="https://github.com/deadpixi/contracts",
