a0efb89dcacb7a6d63e5626acb13f12c89488e5c
Rob King <jking@deadpixi.com>
2016-11-24 23:32:57 -0600

    Explicitly pass args and results to predicates.
    This makes dpcontracts play nicely with linters and such,
    and also avoids some weird and subtle dependencies.
    
    Reworked a lot of the tests to be more accurate.
    
    Added additional assertions, so that invalid contracts are
    detected sooner.
    
    Numerous style and formatting fixes.

--- dpcontracts.py
+++ dpcontracts.py
@@ -3,37 +3,38 @@
 """
 Introduction
 ============
-This module provides a collection of decorators that makes it easy to write
-software using contracts.
+This module provides a collection of decorators that makes it easy to
+write software using contracts.
 
 Contracts are a debugging and verification tool.  They are declarative
-statements about what states a program must be in to be considered "correct"
-at runtime.  They are similar to assertions, but are verified automatically
-at various points in the program.  Contracts can be specified on functions
-and on classes.
+statements about what states a program must be in to be considered
+"correct" at runtime.  They are similar to assertions, and are verified
+automatically at various well-defined points in the program.  Contracts can
+be specified on functions and on classes.
 
-Contracts serve as a form of documentation and a way of formally specifying
-program behavior.  Good practice often includes writing all of the contracts
-first, with these contract specifying the exact expected state before and
-after each function or method call and the things that should always be
-true for a given class of object.
+Contracts serve as a form of documentation and a way of formally
+specifying program behavior.  Good practice often includes writing all of
+the contracts first, with these contract specifying the exact expected
+state before and after each function or method call and the things that
+should always be true for a given class of object.
 
 Contracts consist of two parts: a description and a condition.  The
 description is simply a human-readable string that describes what the
 contract is testing, while the condition is a single function that tests
-that condition.  The condition is executed automatically and with certain
-names predefined, and must return a boolean value: True if the condition
-has been met, and False otherwise.
+that condition.  The condition is executed automatically and passed certain
+arguments (which vary depending on the type of contract), and must return
+a boolean value: True if the condition has been met, and False otherwise.
 
 Preconditions and Postconditions
 ================================
 Contracts on functions consist of preconditions and postconditions.
-A precondition is declared using the `requires` decorator, and describes what
-must be true upon entrance to the function.  Within the condition function,
-the arguments to the decorated function are available.  For example:
+A precondition is declared using the `requires` decorator, and describes
+what must be true upon entrance to the function. The condition function
+is passed an arguments object, which as as its attributes the arguments
+to the decorated function:
 
-    >>> @require("`i` must be an integer", lambda: isinstance(i, int))
-    ... @require("`j` must be an integer", lambda: isinstance(j, int))
+    >>> @require("`i` must be an integer", lambda args: isinstance(args.i, int))
+    ... @require("`j` must be an integer", lambda args: isinstance(args.j, int))
     ... def add2(i, j):
     ...   return i + j
 
@@ -54,16 +55,18 @@ with an AssertionError:
     Traceback (most recent call last):
     AssertionError: `i` must be an integer
 
-Functions can also have postconditions, specified using the `ensures`
-decorator.  Postconditions describe what must be true after the function has
-successfully returned.  Within the condition functions, the special variable
-`__result__` is set to the return value of the function; the values of the
-function arguments are also available.  For example:
+Functions can also have postconditions, specified using the `ensure`
+decorator.  Postconditions describe what must be true after the function
+has successfully returned.  Like the `require` decorator, the `ensure`
+decorator is passed an argument object.  It is also passed an additional
+argument, which is the result of the function invocation.  For example:
 
-    >>> @require("`i` must be a positive integer", lambda: isinstance(i, int) and i > 0)
-    ... @require("`j` must be a positive integer", lambda: isinstance(j, int) and j > 0)
+    >>> @require("`i` must be a positive integer",
+    ...          lambda args: isinstance(args.i, int) and args.i > 0)
+    ... @require("`j` must be a positive integer",
+    ...          lambda args: isinstance(args.j, int) and args.j > 0)
     ... @ensure("the result must be greater than either `i` or `j`",
-    ...         lambda: __result__ > i and __result__ > j)
+    ...         lambda args, result: result > args.i and result > args.j)
     ... def add2(i, j):
     ...     if i == 7:
     ...        i = -7 # intentionally broken for purposes of example
@@ -88,11 +91,11 @@ the function's environments and effects:
     >>> names = set()
     >>> def exists_in_database(x):
     ...   return x in names
-    >>> @require("`name` must be a string", lambda: isinstance(name, str))
+    >>> @require("`name` must be a string", lambda args: isinstance(args.name, str))
     ... @require("`name` must not already be in the database",
-    ...          lambda: not exists_in_database(name.strip()))
+    ...          lambda args: not exists_in_database(args.name.strip()))
     ... @ensure("the normalized version of the name must be added to the database",
-    ...         lambda: exists_in_database(name.strip()))
+    ...         lambda args, result: exists_in_database(args.name.strip()))
     ... def add_to_database(name):
     ...     if name not in names and name != "Rob": # intentionally broken
     ...         names.add(name.strip())
@@ -108,21 +111,22 @@ the function's environments and effects:
 
 All of the various calling conventions of Python are supported:
 
-    >>> @require("`a` is an integer", lambda: isinstance(a, int))
-    ... @require("`b` is a string", lambda: isinstance(b, str))
-    ... @require("every member of `c` should be a boolean", lambda: all(isinstance(x, bool) for x in c))
+    >>> @require("`a` is an integer", lambda args: isinstance(args.a, int))
+    ... @require("`b` is a string", lambda args: isinstance(args.b, str))
+    ... @require("every member of `c` should be a boolean",
+    ...          lambda args: all(isinstance(x, bool) for x in args.c))
     ... def func(a, b="Foo", *c):
     ...     pass
 
     >>> func(1, "foo", True, True, False)
-    >>> func(b="Foo", a=18)
+    >>> func(b="Foo", a=7)
     >>> args = {"a": 8, "b": "foo"}
     >>> func(**args)
     >>> args = (1, "foo", True, True, False)
     >>> func(*args)
-    >>> args = {"a": 8}
+    >>> args = {"a": 9}
     >>> func(**args)
-    >>> func(1)
+    >>> func(10)
 
 A common contract is to validate the types of arguments. To that end,
 there is an additional decorator, `types`, that can be used
@@ -131,21 +135,21 @@ to validate arguments' types:
     >>> class ExampleClass:
     ...     pass
 
-    >>> @types(a=int, b=(str, unicode), c=(ExampleClass, None))
-    ... @require("a must be nonzero", lambda: a != 0)
+    >>> @types(a=int, b=str, c=(type(None), ExampleClass)) # or types.NoneType, if you prefer
+    ... @require("a must be nonzero", lambda args: args.a != 0)
     ... def func(a, b, c=38):
-    ...     print " ".join([a, b])
+    ...     return " ".join(str(x) for x in [a, b])
 
     >>> func(1, "foo", ExampleClass())
-    1 "foo"
+    '1 foo'
 
-    >>> func(1.0, "foo", ExampleClass)
+    >>> func(1.0, "foo", ExampleClass) # invalid type for `a`
     Traceback (most recent call last):
-    AssertionError: type of `a` must be valid
+    AssertionError: the types of arguments must be valid
 
-    >>> func(1, "foo")
+    >>> func(1, "foo") # invalid type (the default) for `c`
     Traceback (most recent call last):
-    AssertionError: type of `c` must be valid
+    AssertionError: the types of arguments must be valid
 
 Contracts on Classes
 ====================
@@ -153,13 +157,13 @@ The `require` and `ensure` decorators can be used on class methods too,
 not just bare functions:
 
     >>> class Foo:
-    ...     @require("`name` should be nonempty", lambda: len(name) > 0)
+    ...     @require("`name` should be nonempty", lambda args: len(args.name) > 0)
     ...     def __init__(self, name):
     ...         self.name = name
 
     >>> foo = Foo()
     Traceback (most recent call last):
-    TypeError: __init__ missing 1 required positional argument: 'name'
+    TypeError: __init__ missing required positional argument: 'name'
 
     >>> foo = Foo("")
     Traceback (most recent call last):
@@ -172,15 +176,21 @@ In this case, "always" means "before invocation of any method and after
 its return" -- methods are allowed to violate invariants so long as they
 are restored prior to return.
 
-The special name `__instance__` is set to a reference to the instance
-variable in question.  For example:
+Invariant contracts are passed a single variable, a reference to the
+instance of the class. For example:
 
-    >>> @invariant("inner list can never be empty", lambda: len(__instance__.lst) > 0)
+    >>> @invariant("inner list can never be empty", lambda self: len(self.lst) > 0)
+    ... @invariant("inner list must consist only of integers",
+    ...            lambda self: all(isinstance(x, int) for x in self.lst))
     ... class NonemptyList:
-    ...     @require("initial list must be a list", lambda: isinstance(initial, list))
-    ...     @require("initial list cannot be empty", lambda: len(initial) > 0)
+    ...     @require("initial list must be a list", lambda args: isinstance(args.initial, list))
+    ...     @require("initial list cannot be empty", lambda args: len(args.initial) > 0)
+    ...     @ensure("the list instance variable is equal to the given argument",
+    ...             lambda args, result: args.self.lst == args.initial)
+    ...     @ensure("the list instance variable is not an alias to the given argument",
+    ...             lambda args, result: args.self.lst is not args.initial)
     ...     def __init__(self, initial):
-    ...         self.lst = initial
+    ...         self.lst = initial[:]
     ...
     ...     def get(self, i):
     ...         return self.lst[i]
@@ -199,6 +209,10 @@ variable in question.  For example:
     >>> nl.pop()
     Traceback (most recent call last):
     AssertionError: inner list can never be empty
+
+    >>> nl = NonemptyList(["a", "b", "c"])
+    Traceback (most recent call last):
+    AssertionError: inner list must consist only of integers
 
 Violations of invariants are ignored in the following situations:
 
@@ -221,27 +235,6 @@ all of the invariants will be tested again:
     >>> nl = NonemptyList([1,2,3])
     >>> nl.as_string() == '1,2,3'
     True
-
-Note that there is a slight difference in the handling of accessing the
-instance variable in methods versus invariants. In invariants, the special
-`__instance__` variable is required, but in methods, one can simply use
-the first argument:
-
-    >>> class AnotherExampleClass:
-    ...     def __init__(self):
-    ...         self._list = []
-    ...
-    ...     @require("x can't already appear in the list", lambda: x not in self._list)
-    ...     @ensure("x has been added to the list", lambda: x in self._list)
-    ...     def add(self, x):
-    ...         self._list.append(x)
-
-    >>> c = AnotherExampleClass()
-    >>> c.add(1)
-    >>> c.add(2)
-    >>> c.add(1)
-    Traceback (most recent call last):
-    AssertionError: x can't already appear in the list
 
 Contracts and Debugging
 =======================
@@ -275,19 +268,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __all__ = ["ensure", "invariant", "require"]
 __author__ = "Rob King"
-__copyright__ = "Copyright (C) 2015 Rob King"
+__copyright__ = "Copyright (C) 2015-2016 Rob King"
 __license__ = "LGPL"
 __version__ = "$Id$"
 __email__ = "jking@deadpixi.com"
 __status__ = "Alpha"
 
+from collections import namedtuple
 from functools import wraps
 from types import FunctionType
 
 try:
     from inspect import getfullargspec
 
-except ImportError:
+except ImportError: # Python 2 compatibility
     from inspect import getargspec
     def getfullargspec(f):
         result = list(getargspec(f))
@@ -296,66 +290,74 @@ except ImportError:
         result.append({})
         return tuple(result)
 
-def build_call(f, *args, **kwargs):
+def get_wrapped_func(func):
+    while hasattr(func, '__contract_wrapped_func__'):
+        func = func.__contract_wrapped_func__
+    return func
+
+def build_call(func, *args, **kwargs):
     """
     Build an argument dictionary suitable for passing via `**` expansion given
     function `f`, positional arguments `args`, and keyword arguments `kwargs`.
     """
 
-    while hasattr(f, '__wrapped_func__'):
-        f = f.__wrapped_func__
-
-    named, vargs, varkw, defs, kwonly, kwonlydefs, _ = getfullargspec(f)
+    func = get_wrapped_func(func)
+    named, vargs, _, defs, kwonly, kwonlydefs, _ = getfullargspec(func)
 
     nonce = object()
     actual = dict((name, nonce) for name in named)
 
-    defs = defs or [] 
+    defs = defs or () 
     kwonlydefs = kwonlydefs or {}
 
-    actual.update(dict(zip(reversed(args), reversed(defs))))
     actual.update(kwonlydefs)
-
-    for name, arg in zip(named, args):
-        actual[name] = arg
+    actual.update(dict(zip(reversed(named), reversed(defs))))
+    actual.update(dict(zip(named, args)))
 
     if vargs:
-        actual[vargs] = args[len(named):]
+        actual[vargs] = tuple(args[len(named):])
 
     actual.update(kwargs)
 
-    for name in named:
-        if actual[name] is nonce:
-            raise TypeError("%s missing 1 required positional argument: '%s'" % (f.__name__, name))
+    for name, value in actual.items():
+        if value is nonce:
+            raise TypeError("%s missing required positional argument: '%s'" % (func.__name__, name))
 
-    return actual
+    return namedtuple('Args', actual.keys())(**actual)
 
-def condition(description, predicate, precondition=False, postcondition=False, method=False):
-    assert isinstance(description, str) and len(description) > 0
-    assert isinstance(predicate, FunctionType)
-    assert hasattr(predicate, "__code__") and hasattr(predicate, "__globals__")
-    assert precondition or postcondition
+def arg_count(func):
+    named, vargs, _, defs, kwonly, kwonlydefs, _ = getfullargspec(func)
+    return len(named) + len(kwonly) + (1 if vargs else 0)
 
+def condition(description, predicate, precondition=False, postcondition=False, instance=False):
+    assert isinstance(description, str), "contract descriptions must be strings"
+    assert len(description) > 0, "contracts must have nonempty descriptions"
+    assert isinstance(predicate, FunctionType), "contract predicates must be functions"
+    assert precondition or postcondition, "contracts must be at least one of pre- or post-conditional"
+    assert arg_count(predicate) == (1 if precondition or instance else 2), \
+           "contract predicates must take the correct number of arguments"
+    
     def require(f):
+        wrapped = get_wrapped_func(f)
+
         @wraps(f)
         def inner(*args, **kwargs):
-            assert not method or len(args) > 0
+            rargs = build_call(f, *args, **kwargs) if not instance else args[0]
 
-            scope = predicate.__globals__.copy()
-            scope.update(build_call(f, *args, **kwargs))
-            if method:
-                scope["__instance__"] = args[0]
-           
             if precondition:
-                assert eval(predicate.__code__, scope), description
+                assert predicate(rargs), description
+
             result = f(*args, **kwargs)
-            if postcondition:
-                scope["__result__"] = result
-                assert eval(predicate.__code__, scope), description
+
+            if instance:
+                assert predicate(rargs), description
+
+            elif postcondition:
+                assert predicate(rargs, result), description
 
             return result
 
-        inner.__wrapped_func__ = f
+        inner.__contract_wrapped_func__ = wrapped
         return inner
     return require
 
@@ -373,16 +375,19 @@ def types(**requirements):
     arguments.
     """
 
-    def predicate():
-        values = locals().copy()
-        for name, kind in requirements.items():
-            assert name in values, "missing required argument `%s`" % name
+    def predicate(args):
+        for name, kind in sorted(requirements.items()):
+            assert hasattr(args, name), "missing required argument `%s`" % name
+
             if not isinstance(kind, tuple):
                 kind = (kind,)
-            assert any(isinstance(values[name], k) or \
-                       (k is None and values[name] is None) for k in kind), \
-                       "type of `%s` must be valid" % name
-    return condition("the types of arguments must be valid", predicate, True, False)
+
+            if not any(isinstance(getattr(args, name), k) for k in kind):
+                return False
+
+        return True
+
+    return condition("the types of arguments must be valid", predicate, True)
 
 def ensure(description, predicate):
     """
@@ -399,16 +404,16 @@ def invariant(description, predicate):
     """
 
     def invariant(c):
-        class Wrapper(c):
+        class InvariantContractor(c):
             pass
 
         for name, value in [(name, getattr(c, name)) for name in dir(c)]:
             if callable(value) and not isinstance(value, type):
                 if name in ("__getitem__", "__setitem__", "__lt__", "__le__", "__eq__",
-                            "__ne__", "__gt__", "__ge__") or not name.startswith("__"):
-                    setattr(Wrapper, name, condition(description, predicate, name != "__init__",
-                                                     True, True)(value))
-        return Wrapper
+                            "__ne__", "__gt__", "__ge__", "__init__") or not name.startswith("__"):
+                    setattr(InvariantContractor, name,
+                            condition(description, predicate, name != "__init__", True, True)(value))
+        return InvariantContractor
     return invariant
 
 if not __debug__:
